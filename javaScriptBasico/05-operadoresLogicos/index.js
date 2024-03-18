bec28251bc5e3f9dc95ca3f2c4e8a278a3f4b35d
Daniel Possamai <daniel.possamai20@gmail.com>
2024-03-18 13:16:02 -0300

    Estudados operadores lógicos e aritimeticos

---
+++ javaScriptBasico/05-operadoresLogicos/index.js
@@ -0,0 +1,29 @@
+/*
+    == --- igual --- a==b verdadeiro se a for igual a b
+    === --- identico --- a===b verdadeiro se a for identico a b
+    != --- diferente --- a!=b verdadeiro se a for diferente de b
+    !== --- não identico --- a!==b verdadeiro se a não for identico a b
+    < --- menor --- a<b verdadeiro se a for menor que b
+    <= --- menor ou igual --- a<=b verdadeiro se a for menor ou igual a b
+    > --- maior --- a>b verdadeiro se a for maior que b
+    >= --- maior ou igual --- a>= verdadeiro se a for maior ou igual a b
+    && --- e --- a === b && a <= b verdadeiro se ambos forem verdadeiro
+    || --- ou --- a === b || a <= b verdadeiro se ao menos um dos dois forem verdadeiro
+    ! --- não(not)(inverter o boleano)
+
+    
+*/
+
+// const a = 3
+// const b = "3"
+
+// console.log(a===b)
+// console.log(a==b)
+
+
+const c = 2
+const d = 2
+
+console.log(c === d && c <= d)
+
+console.log(!(c==d))
---
+++ javaScriptBasico/06-operadoresMatematicos/index.js
@@ -0,0 +1,4 @@
+let primeiroValor = 4
+let segundoValor = 2
+
+console.log(primeiroValor + segundoValor)
