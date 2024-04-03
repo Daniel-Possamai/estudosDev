5def8ca213d44fc140df743a56a9ee1ca449091f
Daniel Possamai <daniel.possamai20@gmail.com>
2024-04-03 08:28:18 -0300

    Aula jsIntermediario começada

---
+++ javaScriptIntermediario/01-escopoJavaScript/index.js
@@ -0,0 +1,22 @@
+// const mensagem = 'Olá'
+
+
+// if(true){
+//     const msg = 'Opa'
+// }
+
+
+// {
+//     var mensagem = 'hop'
+//     console.log(mensagem)
+// }
+
+// console.log(mensagem)
+
+function falar(){
+    var mensagem = 'Olá mundo'
+    console.log(mensagem)
+}
+
+falar()
+console.log(mensagem)
