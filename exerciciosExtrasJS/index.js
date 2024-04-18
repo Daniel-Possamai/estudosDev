d8ee726a621e44294bd0d3a9503a4ebb42f339fe
Daniel Possamai <daniel.possamai20@gmail.com>
2024-04-18 14:03:32 -0300

    realizado exercicio função somar 2 numeros

--- exerciciosExtrasJS/index.js
+++ exerciciosExtrasJS/index.js
@@ -9,3 +9,11 @@
 
 // Exercício 3.2: Escreva uma função que aceite dois números como argumentos e retorne sua soma.
 
+// let numero1 = prompt("Digite um número: ");
+// let numero2 = prompt("Digite outro número: ");
+
+// function somar(n1, n2){
+//     return parseInt(n1) + parseInt(n2);
+// }
+
+// console.log(somar(numero1, numero2));
