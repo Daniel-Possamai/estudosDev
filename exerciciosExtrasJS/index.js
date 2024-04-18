0e8b9487bb129b51a46448c34d925736defa0608
Daniel Possamai <daniel.possamai20@gmail.com>
2024-04-18 13:21:31 -0300

    Exercicios extras para fixação do JS

---
+++ exerciciosExtrasJS/index.js
@@ -0,0 +1,11 @@
+// Exercício 3.1: Escreva um programa que imprima todos os números de 1 a 100.
+
+// let numero = 1;
+
+// for (let i = 0; i < 100; i++){
+//     console.log(numero)
+//     numero++
+// };
+
+// Exercício 3.2: Escreva uma função que aceite dois números como argumentos e retorne sua soma.
+
