dfb66a8a35b937fcf0501b0b514d903a5be6da8b
Daniel Possamai <daniel.possamai20@gmail.com>
2024-04-23 15:31:01 -0300

    Adicionada logica para o botao Nao e fixed para o tamanho do botao nao

--- testes/projectPedido/src/js/index.js
+++ testes/projectPedido/src/js/index.js
@@ -3,8 +3,18 @@ let imgsSushi = divContainter.querySelectorAll('img');
 let btnSim = document.getElementById('btnSim');
 let btnNao = document.getElementById('btnNao');
 
+btnNao.addEventListener('click', ()=>{
+    var newPosY = Math.floor(Math.random() * 500);
+    var newPosX = Math.floor(Math.random() * 500);
+    btnNao.style.position = 'absolute';
+    btnNao.style.left = newPosX + 'px'; 
+    btnNao.style.top = newPosY + 'px'; 
+})
+
+
 btnSim.addEventListener('click', ()=>{
     imgsSushi.forEach(img => {
         img.style.display = 'block';
     });
+    btnNao.style.position = '';
 });
