87b3f46f40869d088d0c528c8b3b0156d918aaff
Daniel Possamai <daniel.possamai20@gmail.com>
2024-04-29 10:54:31 -0300

    interpolando strings com Template Strings

---
+++ javaScriptAvancado/04-interpolacaoDeStrings/index.js
@@ -0,0 +1,15 @@
+// const imprimirInformacoesPessoa = (primeiroNome, ultimoNome, idade) => {
+//     return "Primeiro Nome: " + primeiroNome + " Ultimo Nome: " + ultimoNome +  ". Idade: " + idade;
+// }
+
+// console.log(imprimirInformacoesPessoa("Daniel", "Possamai", 21));
+
+const imprimirInformacoesPessoa = (primeiroNome, ultimoNome, idade) => {
+    return `Primeiro Nome: ${primeiroNome}, Ultimo Nome: ${ultimoNome}, Idade: ${idade}`;
+}
+
+// console.log(imprimirInformacoesPessoa("Daniel", "Possamai", 21));
+
+console.log(`${imprimirInformacoesPessoa("Daneiel", "Possamai", 21)}, ele é um DevQueseter`)
+
+console.log(`A soma é ${10 + 20}`)
