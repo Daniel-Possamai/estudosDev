22197266ba1b4a4dfc0ff68bf2f4f5db9f8e267c
Daniel Possamai <daniel.possamai20@gmail.com>
2024-04-30 11:33:11 -0300

    desestruturando objetos e arrays

---
+++ javaScriptAvancado/05-desestruturacaoJs/index.js
@@ -0,0 +1,22 @@
+// desestruturação de um objeto
+// let pessoa = {nome: 'Daniel', sobrenome: 'Possamai', idade: 21};
+// // let nome = pessoa.nome;
+// // let idade = pessoa.idade;
+
+// // console.log(nome);
+// // console.log(idade);
+
+
+// let { nome: nome, idade: idade } = pessoa;
+
+// console.log(nome);
+// console.log(idade);
+
+//desestruturação de um array
+
+const numeros = [1, 2, 3];
+
+const [um , dois, tres] = numeros;
+
+console.log(um);
+
