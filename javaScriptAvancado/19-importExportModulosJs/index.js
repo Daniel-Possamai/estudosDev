ed42d416c78643e570ab417e390a1a7c870675bf
Daniel Possamai <daniel.possamai20@gmail.com>
2024-06-11 10:28:54 -0300

    Aula realizada sobre importação e exportação de modulos

---
+++ javaScriptAvancado/19-importExportModulosJs/index.js
@@ -0,0 +1,4 @@
+import { somar, multiplicar } from "./operacoesMatematicas.js";
+
+console.log(somar(2, 3))
+console.log(multiplicar(2, 3))
---
+++ javaScriptAvancado/19-importExportModulosJs/operacoesMatematicas.js
@@ -0,0 +1,10 @@
+function somar(numero1, numero2){
+    return numero1 + numero2
+}
+
+
+function multiplicar(numero1, numero2){
+    return numero1 * numero2
+}
+
+export { somar, multiplicar }
