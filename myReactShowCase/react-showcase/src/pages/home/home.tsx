1c401926f2f72878de197cd354f3964a60595db4
Daniel Possamai <daniel.possamai20@gmail.com>
2024-09-26 18:38:45 -0300

    Adicionado titulo a pagina home

--- myReactShowCase/react-showcase/src/pages/home/home.tsx
+++ myReactShowCase/react-showcase/src/pages/home/home.tsx
@@ -1,11 +1,13 @@
 import './home.scss'
 
 
-export default function Home(){
-    return(
-        <div className='test'>
-            hellou papapapa
-            <p>foronfonfon</p>
+export default function Home() {
+    return (
+        <div className='container-home-englobador'>
+            <div className='container-titulo'>
+                <h1>Bem vindo ao ShowCase React</h1>
+                <p>Projetos, e estudos</p>
+            </div>
         </div>
     )
 }
