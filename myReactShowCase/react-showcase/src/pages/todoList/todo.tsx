dd6829905280d31638c5f3f8ac6284d260252215
Daniel Possamai <daniel.possamai20@gmail.com>
2024-11-04 20:34:08 -0300

    Iniciado a estrutura da TODO list

--- myReactShowCase/react-showcase/src/pages/todoList/todo.tsx
+++ myReactShowCase/react-showcase/src/pages/todoList/todo.tsx
@@ -1,11 +1,14 @@
 import './todo.scss'
 
 
-export default function Todo(){
-    return(
-    <div className="test">
-        <p>test</p>
-    </div>
+export default function Todo() {
+    return (
+        <div className="box-todo-englobador">
+            <div className="box-todo">
+
+                <p>test</p>
+            </div>
+        </div>
     )
-    
+
 }
