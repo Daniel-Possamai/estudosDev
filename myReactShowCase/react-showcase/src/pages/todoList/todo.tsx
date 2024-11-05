335e902a7e054b18a8c7f7a5e15ff797dbfa4bd8
Daniel Possamai <daniel.possamai20@gmail.com>
2024-11-04 21:08:50 -0300

    Adicionado material icon ao projeto e adicionado header ao todo list

--- myReactShowCase/react-showcase/src/pages/todoList/todo.tsx
+++ myReactShowCase/react-showcase/src/pages/todoList/todo.tsx
@@ -1,10 +1,16 @@
 import './todo.scss'
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
 
 
 export default function Todo() {
     return (
         <div className="box-todo-englobador">
             <div className="box-todo">
+
+                <div className="head">
+                    <h1>ToDo List</h1>
+                    <AddCircleOutlineIcon className="add-task" />
+                </div>
 
                 <p>test</p>
             </div>
