121c342ba11dd56a82d004d2c7701314780140ed
Daniel Possamai <daniel.possamai20@gmail.com>
2024-12-07 11:10:40 -0300

    componentizado o box para selecioanr qual app abrir

--- myReactShowCase/react-showcase/src/pages/home/home.tsx
+++ myReactShowCase/react-showcase/src/pages/home/home.tsx
@@ -1,18 +1,9 @@
 import './home.scss'
+import BoxSelectApp from '../../components/boxSelectApp/boxSelectApp'
 
 
 export default function Home() {
     return (
-        <div className='container-home-englobador'>
-            <div className='container-titulo'>
-                <h1>Bem vindo ao ShowCase React</h1>
-                <p>Projetos, e estudos</p>
-            </div>
-            <div className="box-pages">
-                <a className="link-page" href="/todo">ToDo List</a>
-                <a className="link-page" href="/todo">Basic Calculator</a>
-                <a className="link-page" href="/todo">App de clima</a>                
-            </div>
-        </div>
+        <BoxSelectApp />
     )
 }
